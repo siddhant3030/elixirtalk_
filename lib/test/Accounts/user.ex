b5ac8adf6cc72175aa69e58ef5862df0e7eb406a
siddhant3030 <ssiddhant3030@gmail.com>
2020-04-25 03:02:55 +0530

    schema defined

--- lib/test/Accounts/user.ex
+++ lib/test/Accounts/user.ex
@@ -1,3 +1,13 @@
 defmodule Test.Accounts.User do
-  alias Ecto.Schema
+  use Ecto.Schema
+  import Ecto.Changeset
+
+
+  schema "user" do
+    field(:email, :string)
+    field(:password, :string)
+    field(:name, :string)
+
+    timestamps(inserted_at: :created_date, updated_at: false)
+  end
 end
