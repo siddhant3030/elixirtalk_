f8a2a14f804a809879a79ce42e9fd114d74e6fc7
siddhant3030 <ssiddhant3030@gmail.com>
2020-04-25 03:19:41 +0530

    migration

---
+++ priv/repo/migrations/20200424214756_create_user.exs
@@ -0,0 +1,11 @@
+defmodule Test.Repo.Migrations.CreateUser do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :name, :string
+      add :email, :string
+      add :password, :string
+      timestamps()
+  end
+end
